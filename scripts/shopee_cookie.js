0dba24dadf5b85bf32e89a80a5a310910c722254
BeeeeeMo <BeeeeeMo@pm.me>
2021-08-27 22:26:32 +0800

    update ✈️

--- scripts/shopee_cookie.js
+++ scripts/shopee_cookie.js
@@ -1,17 +1,17 @@
 if ($request.headers['Cookie']) {
     var headerSP = $request.headers['Cookie'];
     const SPC_EC = headerSP.split('SPC_EC=')[1].split(';')[0];
-    var cookie = $persistentStore.write(headerSP, "CookieSP");
-    var spc_ec = $persistentStore.write(SPC_EC, "SPC_EC");
+    var cookie = $prefs.setValueForKey(headerSP, "CookieSP");
+    var spc_ec = $prefs.setValueForKey(SPC_EC, "SPC_EC");
     var cstfToken = headerSP.split('csrftoken=')[1].split(';')[0];
-    var csrf = $persistentStore.write(cstfToken, "CSRFTokenSP");
+    var csrf = $prefs.setValueForKey(cstfToken, "CSRFTokenSP");
     
     if (!cookie || !csrf || !spc_ec) {
-        $notification.post("蝦皮 Cookie 保存錯誤‼️", "", "請重新登入")
+        $notify("蝦皮 Cookie 保存錯誤‼️", "", "請重新登入")
     } else {
-        $notification.post("蝦皮 Cookie 保存成功🎉", "", "")
+        $notify("蝦皮 Cookie 保存成功🎉", "", "")
     }
 } else {
-    $notification.post("蝦皮 Cookie 保存失敗‼️", "", "請重新登入")
+    $notify("蝦皮 Cookie 保存失敗‼️", "", "請重新登入")
 }
 $done({})
