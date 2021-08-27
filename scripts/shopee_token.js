a6413b582bc86cf8fdd8f28f3f67588ba3bdf543
BeeeeeMo <BeeeeeMo@pm.me>
2021-08-27 22:19:40 +0800

    fix 🔧

--- scripts/shopee_token.js
+++ scripts/shopee_token.js
@@ -2,11 +2,11 @@ if ($request.headers['Cookie']) {
     var shopee_token = $request.headers['Cookie'].split('shopee_token=')[1].split(';')[0];
     var cookie = $prefs.setValueForKey(shopee_token, "ShopeeToken");
     if (!cookie) {
-        $notify.post("蝦皮 Token 保存錯誤‼️", "", "請重新登入")
+        $notify("蝦皮 Token 保存錯誤‼️", "", "請重新登入")
     } else {
-        $notify.post("蝦皮 Token 保存成功🎉", "", "")
+        $notify("蝦皮 Token 保存成功🎉", "", "")
     }
 } else {
-    $notify.post("蝦皮 Token 保存失敗‼️", "", "請重新登入")
+    $notify("蝦皮 Token 保存失敗‼️", "", "請重新登入")
 }
 $done({})
