bc7f207bd49b6e3399cdf03cb5702afac40656e0
BeeeeeMo <BeeeeeMo@gmail.com>
2021-08-31 21:08:04 +0800

    update ✈️

---
+++ scripts/shopee_checkin.js
@@ -0,0 +1,30 @@
+const shopeeUrl = {
+    url: 'https://shopee.tw/mkt/coins/api/v2/checkin',
+    method: "POST", // Optional, default GET.
+    headers:{
+        'Cookie': $prefs.valueForKey("CookieSP") + ';SPC_EC=' + $prefs.valueForKey("SPC_EC") + ';',
+        'X-CSRFToken': $prefs.valueForKey("CSRFTokenSP"),
+    }// Optional.
+};
+
+$task.fetch(shopeeUrl).then(response => {
+    // response.statusCode, response.headers, response.body
+    if (response.statusCode == 200) {
+        let obj = JSON.parse(data);
+        if (obj["data"]["success"]) {
+            var user = obj["data"]["username"];
+            var coins = obj["data"]["increase_coins"];
+            var checkinday = obj["data"]["check_in_day"];
+            $notify("蝦皮 " + user + " 已連續簽到 " + checkinday + " 天", "", "今日已領取 " + coins + "💰💰💰");
+            $done();
+        }
+        $done();
+    } else {
+        $notify("蝦皮 Cookie 已過期‼️", "", "請重新登入 🔓");
+        $done();
+    }
+}, reason => {
+    // reason.error
+    $notify("蝦皮簽到", "", "連線錯誤‼️")
+    $done();
+});
--- scripts/shopee_update_cookie.js
+++ scripts/shopee_update_cookie.js
@@ -1,114 +1,70 @@
 function updateSPC_EC() {
-  const headers = {
-    Cookie: 'shopee_token=' + $prefs.valueForKey('ShopeeToken') + ';'
-  };
-
   const refreshURL = {
-      url: "https://mall.shopee.tw/api/v4/client/refresh",
-      method: "GET",
-      headers: headers,
+    url: 'https://mall.shopee.tw/api/v4/client/refresh',
+    method: "GET", // Optional, default GET.
+    headers: {
+      Cookie:'shopee_token=' + $prefs.valueForKey('ShopeeToken') + ';'
+    }, // Optional.
   };
-  $notify("1");
   $task.fetch(refreshURL).then(response => {
-    // response.statusCode, response.headers, response.body
-    if (response.statusCode == 200) {
+      // response.statusCode, response.headers, response.body
+      if (response.statusCode == 200) {
           let cookie = $prefs.setValueForKey(response.headers['Set-Cookie'].split('SPC_EC=')[1].split(';')[0], 'SPC_EC');
           if (cookie) {
             // $notify('蝦皮 Cookie 保存成功🎉', '', '');
+            console.log('call updateCookie')
             updateCookie();
           } else {
             $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
+            $done();
           }
+      } else {
+          $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
           $done();
-        } else {
-          $notify('蝦皮簽到', '', '連線錯誤‼️');
-          $done();
-        }
-    }, reason => {
-        // reason.error
-        $notify('蝦皮簽到', '', '連線錯誤‼️');
-        $done();
-    });
-  // $httpClient.get(refreshURL, function (error, response, data) {
-  //   if (error) {
-  //     $notify('蝦皮簽到', '', '連線錯誤‼️');
-  //     $done();
-  //   } else {
-  //     if (response.status == 200) {
-  //       let cookie = $persistentStore.write(response.headers['Set-Cookie'].split('SPC_EC=')[1].split(';')[0], 'SPC_EC');
-  //       if (cookie) {
-  //         // $notify('蝦皮 Cookie 保存成功🎉', '', '');
-  //         updateCookie();
-  //       } else {
-  //         $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
-  //       }
-  //       $done();
-  //     } else {
-  //       $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
-  //     }
-  //   }
-  // });
+      }
+  }, reason => {
+      // reason.error
+      $notify('蝦皮簽到', '', '連線錯誤‼️');
+      $done();
+  });
 }
 
 function updateCookie() {
-
-  const headers = {
-    Cookie:
-    $prefs.valueForKey('CookieSP') + ';SPC_EC=' + $prefs.valueForKey('SPC_EC') +';',
-  'X-CSRFToken': $prefs.valueForKey('CSRFTokenSP'),
-  };
-  $notify("2");
   const accountInfoURL = {
       url: 'https://shopee.tw/api/v2/user/account_info?from_wallet=false&skip_address=1&need_cart=1',
-      method: "GET",
-      headers: headers,
+      method: 'GET', // Optional, default GET.
+      headers: {
+        Cookie:
+          $prefs.valueForKey('CookieSP') + ';SPC_EC=' + $prefs.valueForKey('SPC_EC') +';',
+          'X-CSRFToken': $prefs.valueForKey('CSRFTokenSP'),
+      }, // Optional.
   };
 
   $task.fetch(accountInfoURL).then(response => {
-    // response.statusCode, response.headers, response.body
-    if (response.statusCode == 200) {
-      let cookie = $prefs.setValueForKey(
-        response.headers['Set-Cookie'],
-        'CookieSP'
-      );
-      if (cookie) {
-        $notify('蝦皮 Cookie 保存成功🎉', '', '');
+      // response.statusCode, response.headers, response.body
+      if (response.statusCode == 200) {
+        let cookie = $prefs.setValueForKey(
+          response.headers['Set-Cookie'],
+          'CookieSP'
+        );
+        if (cookie) {
+          console.log('updateCookie')
+          $notify('蝦皮 Cookie 保存成功🎉', '', '');
+        } else {
+          $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
+        }
+        $done();
       } else {
         $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
       }
       $done();
-    } else {
-      $notify('蝦皮簽到', '', '連線錯誤‼️');
-      $done();
-    }
   }, reason => {
-    // reason.error
-    $notify('蝦皮簽到', '', '連線錯誤‼️');
-    $done();
+      // reason.error
+      $notify('蝦皮簽到', reason.error, '連線錯誤‼️');
+      $done();
   });
-  
-
-  // $httpClient.get(accountInfoURL, function (error, response, data) {
-  //   if (error) {
-  //     $notify('蝦皮簽到', '', '連線錯誤‼️');
-  //     $done();
-  //   } else {
-  //     if (response.status == 200) {
-  //       let cookie = $persistentStore.write(
-  //         response.headers['Set-Cookie'],
-  //         'CookieSP'
-  //       );
-  //       if (cookie) {
-  //         $notify('蝦皮 Cookie 保存成功🎉', '', '');
-  //       } else {
-  //         $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
-  //       }
-  //       $done();
-  //     } else {
-  //       $notify('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
-  //     }
-  //   }
-  // });
 }
 
 updateSPC_EC();
+
+
